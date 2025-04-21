d613745f5c999253824d17c2835efc8bdd48dc97
古明地こいし <zimamaxiaoan@outlook.com>
2025-04-21 13:22:45 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -84,37 +84,30 @@ const API_SITES = {
     jkm3u8: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: false
     },
     bwm3u8: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: false
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: false
     },
     siwa: {
         api: 'https://siwazyw.tv',
         name: '丝袜资源',
-        adult: false
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: false
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: false
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: false,
         detail: 'https://hsckzy.vip'
     },
     zuid: {
